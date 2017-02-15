8b5809fc8fa00e93c87bd0f4d40d85e5f580ee4b
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2017-02-16 02:12:00 +0330

    Install if config files not found.

--- index.php
+++ index.php
@@ -3,17 +3,15 @@ define ( "PLUF_BASE", dirname ( __FILE__ ) . '/vendor/pluf/core/src' );
 define ( "SRC_BASE", dirname ( __FILE__ ) . '/storage' );
 
 require 'vendor/autoload.php';
-require 'Pluf.php';
 
 /*
  * NOTE: Remove if block in real installation.
  */
 if (!file_exists('config.php')) {
     include dirname ( __FILE__ ) . '/vendor/pluf/installer/index.php';
-    return;
+} else {
+    require 'Pluf.php';
+    Pluf::start ('config.php' );
+    Pluf_Dispatcher::loadControllers ( 'urls.php' );
+    Pluf_Dispatcher::dispatch ( Pluf_HTTP_URL::getAction () );
 }
-
-Pluf::start ('config.php' );
-Pluf_Dispatcher::loadControllers ( 'urls.php' );
-Pluf_Dispatcher::dispatch ( Pluf_HTTP_URL::getAction () );
-
