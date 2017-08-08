6ed3467c1a18e4879ed7ab8d0cce4a2b18a48e9d
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2017-08-08 20:36:55 +0430

    Adding shop as sub-module.
    Bug fixed: append SPA manager API as default into the end of list.

--- urls.php
+++ urls.php
@@ -5,147 +5,154 @@ $base = '';
  * General apis
  */
 $api = array(
-        array (
+        array(
                 'app' => 'User',
                 'regex' => '#^/api/user#',
                 'base' => $base,
                 'sub' => include 'User/urls.php'
         ),
-        array (
+        array(
                 'app' => 'Group',
                 'regex' => '#^/api/group#',
                 'base' => $base,
                 'sub' => include 'Group/urls.php'
         ),
-        array (
+        array(
                 'app' => 'Role',
                 'regex' => '#^/api/role#',
                 'base' => $base,
                 'sub' => include 'Role/urls.php'
         ),
-        array ( // CMS : Content Management System
+        array( // CMS : Content Management System
                 'app' => 'CMS',
                 'regex' => '#^/api/cms#',
                 'base' => $base,
                 'sub' => include 'CMS/urls.php'
         ),
-        array (
+        array(
                 'app' => 'Spa',
                 'regex' => '#^/api/spa#',
                 'base' => $base,
                 'sub' => include 'Spa/urls.php'
         ),
-        array (
+        array(
                 'app' => 'Message',
                 'regex' => '#^/api/message#',
                 'base' => $base,
                 'sub' => include 'Message/urls.php'
         ),
-        array ( // Tenant configuration
+        array( // Tenant configuration
                 'app' => 'Setting',
                 'regex' => '#^/api/setting#',
                 'base' => $base,
                 'sub' => include 'Setting/urls.php'
         ),
-        array ( // Seo
+        array( // Seo
                 'app' => 'Seo',
                 'regex' => '#^/api/seo#',
                 'base' => $base,
                 'sub' => include 'Seo/urls-seen-v1.php'
         ),
-        array (
+        array(
                 'app' => 'Assort',
                 'regex' => '#^/api/assort#',
                 'base' => $base,
                 'sub' => include 'Assort/urls.php'
-        ),
-        
+        )
+
 );
 
 /*
  * Special apis
  */
 $spec = array(
-        array (
+        array(
                 'app' => 'Tenant',
                 'regex' => '#^/api/tenant#',
                 'base' => $base,
                 'sub' => include 'Tenant/urls.php'
         ),
-        array (
+        array(
                 'app' => 'Book',
                 'regex' => '#^/api/wiki#',
                 'base' => $base,
                 'sub' => include 'Book/urls.php'
         ),
-        array (
+        array(
                 'app' => 'Monitor',
                 'regex' => '#^/api/monitor#',
                 'base' => $base,
                 'sub' => include 'Monitor/urls.php'
         ),
-        array (
+        array(
                 'app' => 'SaaSDM',
                 'regex' => '#^/api/dm#',
                 'base' => $base,
                 'sub' => include 'SaaSDM/urls.php'
         ),
-        array (
+        array(
                 'app' => 'SDP',
                 'regex' => '#^/api/sdp#',
                 'base' => $base,
                 'sub' => include 'SDP/urls.php'
         ),
-        array ( // Tenant configuration
+        array( // Tenant configuration
                 'app' => 'Config',
                 'regex' => '#^/api/config#',
                 'base' => $base,
                 'sub' => include 'Config/urls.php'
         ),
-        array ( // Bank
+        array( // Bank
                 'app' => 'Bank',
                 'regex' => '#^/api/bank#',
                 'base' => $base,
                 'sub' => include 'Bank/urls.php'
         ),
-        array ( // Book
+        array( // Book
                 'app' => 'Book',
                 'regex' => '#^/api/book#',
                 'base' => $base,
                 'sub' => include 'Book/urls.php'
         ),
-        array ( // Calender
+        array( // Calender
                 'app' => 'Calendar',
                 'regex' => '#^/api/calendar#',
                 'base' => $base,
                 'sub' => include 'Calendar/urls.php'
         ),
-        array ( // Backup
+        array( // Backup
                 'app' => 'Backup',
                 'regex' => '#^/api/backup#',
                 'base' => $base,
                 'sub' => include 'Backup/urls.php'
         ),
-        array (
+        array(
                 'app' => 'Collection',
                 'regex' => '#^/api/collection#',
                 'base' => $base,
                 'sub' => include 'Collection/urls.php'
         ),
-        array (
-                'app' => 'Spa',
-                'regex' => '#^#',
+        array(
+                'app' => 'Shop',
+                'regex' => '#^/api/shop#',
                 'base' => $base,
-                'sub' => include 'Spa/urls-app2.php'
+                'sub' => include 'Shop/urls.php'
         )
 );
 
-foreach ($spec as $moduleApi){
-    if(Config_Service::get('module.'.$moduleApi['app'].'.enable', FALSE)){
+foreach ($spec as $moduleApi) {
+    if (Config_Service::get('module.' . $moduleApi['app'] . '.enable', FALSE)) {
         array_push($api, $moduleApi);
     }
 }
 
+array_push($api,
+        array(
+                'app' => 'Spa',
+                'regex' => '#^#',
+                'base' => $base,
+                'sub' => include 'Spa/urls-app2.php'
+        ));
 return $api;
 
 
