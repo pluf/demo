2a60a9a82b8c50452c673e26250974c235bbe3a9
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2018-01-10 01:53:46 +0330

    Bug fixed: created link is not valid. ln command is updated

--- link.sh
+++ link.sh
@@ -6,6 +6,6 @@ for file in `find . -maxdepth 1 -mindepth 1 -type d`
 do
 	package=`basename $file`
 	rm -fR "$file"
-	ln -s "~/git/pluf/${package}"
+	ln -s ~/git/pluf/${package}
 	echo "$package		.......................[ok]"
 done
