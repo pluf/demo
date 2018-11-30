cef894ae22c76d0f87b4b87eb90bd14f7350d9cb
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2018-11-30 15:32:00 +0330

    Manage db with flywaydb

---
+++ docker/dgyar/dgyar.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+attempt_counter=0
+max_attempts=5
+
+# Migrate db
+cd "/var/www"
+./flyway validate
+exit_status=$?
+echo $exit_status 
+until [ $exit_status -eq 1 ] || [ $exit_status -eq 0 ]; do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached"
+      exit 1
+    fi
+    printf '.'
+    attempt_counter=$(($attempt_counter+1))
+    sleep 60
+    
+	./flyway validate
+	exit_status=$?
+	echo $exit_status 
+done
+./flyway -X migrate
+
+
+# Run Apcache2 froground application
+cd "/var/www/html"
+apache2-foreground
